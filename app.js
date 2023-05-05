4cb55b60b2ef6b2a31507cedad2203ed93c63459
SzymonB5 <235831@edu.p.lodz.pl>
2023-05-05 20:57:02 +0200

    Stage 2

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
-
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
 
@@ -17,29 +16,27 @@ app.get('/', (req, res) => {
         // Generate an OAuth URL and redirect there
         const url = oAuth2Client.generateAuthUrl({
             access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/gmail.readonly'
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
         });
         console.log(url)
         res.redirect(url);
     } else {
-        const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-        gmail.users.labels.list({
-            userId: 'me',
-        }, (err, res) => {
-            if (err) return console.log('The API returned an error: ' + err);
-            const labels = res.data.labels;
-            if (labels.length) {
-                console.log('Labels:');
-                labels.forEach((label) => {
-                    console.log(`- ${label.name}`);
-                });
+        var oauth2 = google.oauth2({auth: oAuth2Client, version: 'v2'});
+        oauth2.userinfo.v2.me.get(function (err, result) {
+            if (err) {
+                console.log('ERROR');
+                console.log(err);
             } else {
-                console.log('No labels found.');
+                loggedUser = result.data.name;
+                console.log(loggedUser);
             }
-        });
-        res.send('Logged in')
+            res.send('Logged in: '.concat(loggedUser, ' <img src = "', result.data.picture, '" height="23" width="23"> <br> <a href="#" onclick="signOut();">Sign out</a>' +
+                '<script> function signOut() { var auth2 = gapi.auth2.getAuthInstance(); auth2.signOut().then(function () {' +
+                'console.log("User signed out."); window.location.replace("https://lab6-zad3.onrender.com/");' +
+                '});}</script>'));
+        })
     }
-})
+});
 
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
