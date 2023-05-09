01628436a2661fad56e98b10fefafd02d18c457e
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-05-09 16:48:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
             }
             res.send('Logged in: '.concat(loggedUser, ' <img src = "', result.data.picture, '" height="23" width="23"> <br> <a href="#" onclick="signOut();">Sign out</a>' +
                 '<script> function signOut() { var auth2 = gapi.auth2.getAuthInstance(); auth2.signOut().then(function () {' +
-                'console.log("User signed out."); authed = false; window.location.replace("/");' +
+                'console.log("User signed out."); authed = false; window.location.replace("/login");' +
                 '});}</script>'));
         })
     }
